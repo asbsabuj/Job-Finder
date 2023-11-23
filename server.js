ff9f67fb58a6dae46fc9d0761b17e176a5d24503
Sabuj <asbsabuj1214@gmail.comgit config --global user.name Sabujgit commit -m 'first push'git config -global color.ui autogit config --global user.email asbsabuj1214@gmail.com>
2023-11-24 00:38:10 +0600

    production_3

--- server.js
+++ server.js
@@ -1,47 +1,39 @@
-import "express-async-errors";
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./db/connect.js";
-import notFoundMiddleware from "./middleware/not-found.js";
-import errorHandlerMiddleware from "./middleware/error-handler.js";
-import authRouter from "./routes/authRoutes.js";
-import jobsRouter from "./routes/jobsRoutes.js";
-import morgan from "morgan";
+import "express-async-errors"
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./db/connect.js"
+import notFoundMiddleware from "./middleware/not-found.js"
+import errorHandlerMiddleware from "./middleware/error-handler.js"
+import authRouter from "./routes/authRoutes.js"
+import jobsRouter from "./routes/jobsRoutes.js"
+import morgan from "morgan"
 
-const app = express();
+const app = express()
 
-dotenv.config();
+dotenv.config()
 
-app.use(express.json());
+app.use(express.json())
 
 if (process.env.NODE_ENV !== "production") {
-  app.use(morgan("dev"));
+  app.use(morgan("dev"))
 }
 
-app.get("/", (req, res) => {
-  res.json({ msg: "welcome" });
-});
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/jobs", jobsRouter)
 
-app.get("/api/v1", (req, res) => {
-  res.json({ msg: "API" });
-});
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
-
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL);
+    await connectDB(process.env.MONGO_URL)
     app.listen(port, () => {
-      console.log(`server listening on port ${port}...`);
-    });
+      console.log(`server listening on port ${port}...`)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
-start();
+}
+start()
